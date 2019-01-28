b5bc85043e9d569604d3f1b4eac1f8092b9215f9
hminkoo92 <dakku92@me.com>
2019-01-28 17:41:09 +0900

    Add create_flow()

--- TrafficManagement.c
+++ TrafficManagement.c
@@ -27,7 +27,7 @@
 #include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <stdbool.h>
-#include 
+
 // Global variables
 unsigned long int total=0;
 ipAddr[256][16];
@@ -44,11 +44,12 @@ typedef struct FlowNode
 	struct FlowNode *next;
 } FlowNode;
 
-// Flow head
-FlowNode* head;
+// Head flow and current flow
+FlowNode* head = NULL;
+FlowNode* curr = NULL;
 
 // Create, Insert, Remove, Search function for flow management
-FlowNode *create_node(char *addr)
+FlowNode *create_flow(char *addr)
 {
 	// Allocate memory for flow node
 	FlowNode *new_flow;
@@ -56,12 +57,28 @@ FlowNode *create_node(char *addr)
 	if ( new_flow == NULL) {
 		error("Memory allocation error");
 	}
+
+	// Attach new node to end of flow list
+	FlowNode *p = head;
+	if (p == NULL){ // There is no any flow in Flow list, attach flow node as first node
+		head = new_flow;
+		new_flow->next = NULL;		
+	}
+
+	while ( p != NULL ){
+
+		if ( p->next == NULL ){
+			p->next = new_flow;
+			new_flow->next = NULL;
+		}
+		p = p->next;
+	}
 	
 	// Initialization for flow node
 	new_flow->addr = addr;
-	uplink = 0;
-	downlink = 0;
-	app = 0;
+	new_flow->uplink = 0;
+	new_flow->downlink = 0;
+	new_flow->app = 0;
 }
 
 FlowNode *search_flow (FlowNode *head, char *addr)
@@ -89,9 +106,10 @@ int main(int argc, char const *argv[])
 	char errbuf[100] , *devname , devs[100][100];
 	int count = 1 , n;
 
-	// Initialize IP Arrays 192.168.1.0 - 192.168.1.255
+	// Initialize IP Arrays 192.168.1.0 - 192.168.1.255 to implement in HG (10.0.2.0 - 10.0.2.255 for local test)
 	for ( int i = 0; i < 256; ++i ){
-		sprintf(ipAddr[i], "192.168.1.%d", i);
+		//sprintf(ipAddr[i], "192.168.1.%d", i); // for ip for home gateway
+		sprintf(ipAddr[i], "10.0.2.%d", i);
 		printf("%s\n", ipAddr[i]);
 	}
 
@@ -131,7 +149,7 @@ int main(int argc, char const *argv[])
 	}
 	printf("Done\n");
 
-	// Record fragment information
+	// Record results
 	logfile = fopen("log.json","w");
 	if(logfile == NULL)
 	{
@@ -147,10 +165,28 @@ int main(int argc, char const *argv[])
 }
 
 // Flow update
-void flow_update()
+void analyze_flow(FlowNode *flow, char* addr, int packetSize, const u_char *buffer, bool isUplink)
 {
-
+	if ( isUplink == true ){ // Uplink
+		// Todo: Update basic flow inforamtion EX) Throughput, etc.
+		printf("[Uplink]   Currnet Flow Info.\n");
+		printf("IP: %s\n", curr->addr);
+		printf("Uplink: %d\n", curr->uplink);
+		printf("Downlink: %d\n", curr->downlink);
+		printf("------------------------------------------------\n");
+		// Todo: Analyze flows to identify app used by flow
+	}
+	else{ // Downlink
+		// Todo: Update basic flow's information
+		printf("[Downlink] Currnet Flow Info.\n");
+		printf("IP: %s\n", curr->addr);
+		printf("Uplink: %d\n", curr->uplink);
+		printf("Downlink: %d\n", curr->downlink);
+		printf("------------------------------------------------\n");
+		// Todo: Analyze flows to identify app used by flow
+	}
 }
+
 // Process packet 
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer)
 {
@@ -172,22 +208,30 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 	// Check flow list
 	for ( int i = 0; i < 256; ++i ){
 		if ( !strcmp( inet_ntoa(source.sin_addr), ipAddr[i]) ){ // Uplink 
-			if( search_flow(head, ipAddr[i]) != NULL ){ 
-				// Todo: Update flow information
+			if( (curr = search_flow(head, ipAddr[i])) != NULL ){ 
+				// Update flow information
+				analyze_flow(ipAddr[i], size, buffer, true);
 			}
 			else{
-				// Todo: Create flow node and Update flow information
+				// Create flow node and Update flow information
+				create_flow(ipAddr[i]);
+				analyze_flow(ipAddr[i], size, buffer, false);
 			}
 		}
 		else if ( !strcmp( inet_ntoa(dest.sin_addr), ipAddr[i]) ){ // Downlink
-			if( search_flow(head, ipAddr[i]) != NULL ){ 
-				// Todo: Update flow information
-
+			if( (curr = search_flow(head, ipAddr[i])) != NULL ){ 
+				// Update flow information
+				analyze_flow(ipAddr[i], size, buffer, true);
 			}
 			else{
-				// Todo: Create flow node and Update flow information
+				// Create flow node and Update flow information
+				create_flow(ipAddr[i]);
+				analyze_flow(ipAddr[i], size, buffer, false);
 			}
 		}
 	}
+
+	// After flow analysis, we can get flow's application. so, we can do traffic shaping according to flow's applicaiton
+	/* Code */
 }
 
